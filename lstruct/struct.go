974bb608307539fcb88f2dd95b8a5c9c3744f4dc
MLGBrotishka <79323260+MLGBrotishka@users.noreply.github.com>
2023-07-07 19:58:33 +0300

    Struct resolve

--- lstruct/struct.go
+++ lstruct/struct.go
@@ -10,9 +10,8 @@ type Courier struct {
 	Position Coordinate `json:"position"`
 }
 
-type PathInfo struct {
-	CourierID int          `json:"courier-id"`
-	Path      []Coordinate `json:"path"`
-	Time      int          `json:"time"`
-	Cost      float64      `json:"cost"`
+type Point struct {
+	Lon    float64 `json:"lon"`
+	Lat    float64 `json:"lat"`
+	Chunks []int
 }
