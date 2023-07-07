a2a752a45b92c4286de973fbfa822ef34704f8f9
MLGBrotishka <79323260+MLGBrotishka@users.noreply.github.com>
2023-07-07 23:59:31 +0300

    codestyle commit

--- handlers/v1-point-is-available-get.go
+++ handlers/v1-point-is-available-get.go
@@ -19,13 +19,13 @@ func GetV1PointIsAvailable(w http.ResponseWriter, r *http.Request) {
 	}
 
 	res := lstruct.IsCorrectCoordinate(coordinate)
-	if (res != 0) {
+	if res != 0 {
 		var errorResponse lstruct.ErrorResponse
-		if (res == 1) {
+		if res == 1 {
 			errorResponse = lstruct.ErrorResponse{
 				Message: "Longitude out of range",
 			}
-		} else if (res == 2) {
+		} else if res == 2 {
 			errorResponse = lstruct.ErrorResponse{
 				Message: "Latitude out of range",
 			}
@@ -33,7 +33,7 @@ func GetV1PointIsAvailable(w http.ResponseWriter, r *http.Request) {
 		SendJSONResponse(w, http.StatusBadRequest, errorResponse)
 		return
 	}
-  
+
 	// Проверка принадлежности координат диапазону
 	available := coordinate.Lon >= -90 && coordinate.Lon <= 90 && coordinate.Lat >= -90 && coordinate.Lat <= 90
 
